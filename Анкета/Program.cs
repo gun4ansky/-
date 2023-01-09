c24f9387da2cebf7be6b62e929cd768ef9604e47
gun4ansky <gun_project@mail.ru>
2023-01-09 17:39:41 +0500

    Добавьте файлы проекта.

---
+++ Анкета/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Анкета
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте, как вас зовут?");
+            string name = (Console.ReadLine());
+            Console.WriteLine("Сколько вам лет?");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваша дата рождения?");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Вас зовут {0}, вам {1} лет, вы родились {2},", name, age, birthdate);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
